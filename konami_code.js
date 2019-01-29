51ff1188335ca317b86852b3a1a2e82e0c832da3
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-29 19:39:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+ let index = 0;
+ 
 function init() {
   // Write your JavaScript code inside the init() function
-  let index = 0;
  
   // This is the function that would be invoked by the event listener.
   function onKeyDownHandler(e) {
